cfb360a6b72107d3d01f635b64bc4a081a47a3ae
sandeep-skb <31074448+sandeep-skb@users.noreply.github.com>
2019-06-15 15:47:54 -0700

    updating the bfs algo

--- Graphs/breadth_first_traversal.py
+++ Graphs/breadth_first_traversal.py
@@ -1,9 +1,9 @@
-class Stack:
+class Queue:
   def __init__(self):
     self.l = []
   
   def push(self, val):
-    self.l.insert(0, val)
+    self.l.append(val)
   
   def pop(self):
     return(self.l.pop(0))
@@ -18,7 +18,7 @@ class Graph:
   def __init__(self, num_vertices):
     self.num_vtx = num_vertices
     self.visited = [0]*num_vertices
-    self.stack = Stack()
+    self.queue = Queue()
     self.adj_mtx = []
     for _ in range(self.num_vtx):
       self.adj_mtx.append([0]*self.num_vtx)
@@ -29,15 +29,17 @@ class Graph:
   
   
   def breadth_first_search(self, vtx):
-    #print("pushing into the stack: ", vtx)
-    self.stack.push(vtx)
-    while(self.stack.isEmpty() == False):
-      cur_vtx = self.stack.pop()
+    #print("pushing into the queue: ", vtx)
+    self.queue.push(vtx)
+    while(self.queue.isEmpty() == False):
+      cur_vtx = self.queue.pop()
+      print(cur_vtx)
       self.visited[cur_vtx] = 1
       for i in range(0, self.num_vtx):
         if (self.adj_mtx[cur_vtx][i] == 1 and self.visited[i] == 0):
-          print("Traversing from: {} -> {}".format(cur_vtx, i))
-          self.stack.push(i)
+          self.visited[i] = 1
+          #print("cur_vtx: {} vtx pushing: {}".format(cur_vtx, i))
+          self.queue.push(i)
       
 
 
@@ -53,6 +55,21 @@ def main():
   g.add_edge(3,6)
   g.add_edge(4,7)
   print("Doing Breadth First Search Traversal")
+'''
+  0
+  |
+  v
+  1--> 2
+  |    |
+  v    v
+  3--> 4--> 5
+  |    |
+  v    v
+  6    7
+'''
+
+
+
   g.breadth_first_search(0)
 
 if __name__ == "__main__":
