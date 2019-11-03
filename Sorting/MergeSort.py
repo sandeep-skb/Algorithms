fb89a255fbc8e3d6a18d0f23646545badfa02687
sandeep-skb <31074448+sandeep-skb@users.noreply.github.com>
2019-11-03 11:02:45 -0800

    Updating MergeSort

--- Sorting/MergeSort.py
+++ Sorting/MergeSort.py
@@ -1,35 +1,23 @@
 def mergeSort(arr):
   if len(arr) > 1:
-    middle = len(arr) // 2
-    leftArr = arr[:middle]
-    rightArr = arr[middle:]
-    mergeSort(leftArr)
-    mergeSort(rightArr)
+    mid = len(arr)//2
+    left = mergeSort(arr[:mid])
+    right = mergeSort(arr[mid:])
 
-    i = j = k = 0
-    
-    while(i < len(leftArr) and j < len(rightArr)):
-      if leftArr[i] < rightArr[j]:
-        arr[k] = leftArr[i]
-        i += 1
+    l = r = i = 0
+    while(l < len(left) and r < len(right)):
+      if left[l] < right[r]:
+        arr[i] = left[l]
+        l += 1
       else:
-        arr[k] = rightArr[j]
-        j += 1
-      k += 1
-    
-    while(i < len(leftArr)):
-      arr[k] = leftArr[i]
+        arr[i] = right[r]
+        r += 1
       i += 1
-      k += 1
-    while(j < len(rightArr)):
-      arr[k] = rightArr[j]
-      j += 1
-      k += 1
-    print("left: ", leftArr, " right: ", rightArr, " --> ", arr, )
     
+    arr[i:] = left[l:] + right[r:]
+  
+  return arr
 
-arr = [4,2,1,5,3,9]
-leftStart = 0 
-rightEnd = len(arr)
-mergeSort(arr)
-print("final: ", arr)
+
+arr = [60, 40, 50, 20, 10, 3, 80, 100]
+print(mergeSort(arr))
